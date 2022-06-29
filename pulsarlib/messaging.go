a42a7f2aaeb15407543b44c226174216b785e258
Sagar Rohankar <sagar.rohankar@blueshift.com>
2022-06-29 17:24:52 +0530

    Define NACK re-delivery time to 1 second (default is 1 min)

--- pulsarlib/messaging.go
+++ pulsarlib/messaging.go
@@ -362,11 +362,12 @@ func CreateConsumer(tenantID string, namespace string, topics []string, subscrip
 		topicArr = append(topicArr, fmt.Sprintf("persistent://%s/%s/%s", tenantID, namespace, tp))
 	}
 	c, err := msging.client.Subscribe(pulsar.ConsumerOptions{
-		Topics:           topicArr,
-		SubscriptionName: subscriptionName,
-		Type:             pulsar.Shared,
-		RetryEnable:      true,
-		DLQ:              dlqPolicy,
+		Topics:              topicArr,
+		SubscriptionName:    subscriptionName,
+		Type:                pulsar.Shared,
+		NackRedeliveryDelay: 1 * time.Second,
+		RetryEnable:         true,
+		DLQ:                 dlqPolicy,
 	})
 	if err != nil {
 		return nil, fmt.Errorf("Error in subscribing to the topics. Error %v", err)
